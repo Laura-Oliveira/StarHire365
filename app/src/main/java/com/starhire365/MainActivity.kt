58906b959a1a777e1eb8a8ff6b552e8cab25d54a
laura <laura.regina.cp@gmail.com>
2021-11-02 15:29:29 -0300

    Exercício sobre mensagens de Log

--- app/src/main/java/com/starhire365/MainActivity.kt
+++ app/src/main/java/com/starhire365/MainActivity.kt
@@ -2,11 +2,28 @@ package com.starhire365
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        val high:Double = 1.70
+        var age:Int = 27
+        var occupation:String = "Software Engineering"
+
+        Log.d("SplashScreen", "Executing on-create: Occupation: $occupation [Debug]")
+        Log.i("SplashScreen", "Executing on-create: Occupation: $occupation [Information]")
+        Log.v("SplashScreen", "Executing on-create: Occupation: $high [Verbose]")
+        Log.w("SplashScreen", "Executing on-create: Occupation: $age [Warning]")
+        Log.e("SplashScreen", "Executing on-create: Occupation: $age [Error]")
+    }
 }
+
+
+
+
+
